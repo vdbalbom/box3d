f111ca53794d2e8629c7d1306670ccaff405ea4b
Virginia Monnerat <virginiamonnerat@gmail.com>
2017-09-05 21:39:46 -0300

    add collision with box to character controller

--- character_controller.lua
+++ character_controller.lua
@@ -1,23 +1,68 @@
 local contants = require("global")
 
+function checkBoxCollision()
+  p = getPlayer()
+  b = getBox()
+  if p.position.x > b.size.width - p.size.width/2 then
+    return true
+  elseif p.position.x < 0 + p.size.width/2 then
+    return true
+  elseif p.position.z > b.size.depth then
+    return true
+  elseif p.position.z < 0 + p.size.depth then
+    return true
+  elseif p.position.y > b.size.height - p.size.height then
+    return true
+  elseif p.position.y < 0 then
+    return true
+  else
+    return false
+  end
+end
+
+function checkCollision()
+  return checkBoxCollision()
+end
+
 function updateCharacter()
 
   p = getPlayer()
 
   if love.keyboard.isDown(RIGHT) then
     p.position.x = p.position.x + p.speed*DT
+    if checkCollision() then
+      p.position.x = p.position.x - p.speed*DT
+    end
   end
   if love.keyboard.isDown(LEFT) then
     p.position.x = p.position.x - p.speed*DT
+    if checkCollision() then
+      p.position.x = p.position.x + p.speed*DT
+    end
   end
   if love.keyboard.isDown(UP) then
     p.position.z = p.position.z - p.speed*DT/2
+    if checkCollision() then
+      p.position.z = p.position.z + p.speed*DT/2
+    end
   end
   if love.keyboard.isDown(DOWN) then
     p.position.z = p.position.z + p.speed*DT/2
+    if checkCollision() then
+      p.position.z = p.position.z - p.speed*DT/2
+    end
   end
   if love.keyboard.isDown("z") then
-
+    p.position.y = p.position.y - p.speed*DT
+    if checkCollision() then
+      p.position.y = p.position.y + p.speed*DT
+    end
+  end
+  if love.keyboard.isDown("a") then
+    p.position.y = p.position.y + p.speed*DT
+    if checkCollision() then
+      p.position.y = p.position.y - p.speed*DT
+    end
   end
 end
 
