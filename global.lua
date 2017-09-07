7d331f340843adcd8150c8b01434bb973ff854fe
Virginia Monnerat <virginiamonnerat@gmail.com>
2017-09-06 23:28:48 -0300

    put gravity function into global file

--- global.lua
+++ global.lua
@@ -32,3 +32,57 @@ function getLevel()
   require("sample/levels/" .. CURRENT_LEVEL .. "/info")
   return level
 end
+
+function gravity(object, box)
+
+  object.gravity_time = object.gravity_time + DT
+
+  posi= box.gravity.acceleration*object.gravity_time*object.gravity_time
+
+  if box.gravity.surface == "down" then
+    object.position.y = object.position.y - posi
+    if checkCollision() then
+      object.position.y = object.position.y + posi
+      object.gravity_time = 0
+    end
+    if love.keyboard.isDown("a") and object.speed["y+"] > 0 then
+      object.gravity_time = 0
+    end
+  elseif box.gravity.surface == "up" then
+    object.position.y = object.position.y + posi
+    if checkCollision() then
+      object.position.y = object.position.y - posi
+      object.gravity_time = 0
+    end
+    if love.keyboard.isDown("z") and object.speed["y-"] > 0 then
+      object.gravity_time = 0
+    end
+  elseif box.gravity.surface == "left" then
+    object.position.x = object.position.x - posi
+    if checkCollision() then
+      object.position.x = object.position.x + posi
+      object.gravity_time = 0
+    end
+    if love.keyboard.isDown("right") and object.speed["x+"] > 0 then
+      object.gravity_time = 0
+    end
+  elseif box.gravity.surface == "right" then
+    object.position.x = object.position.x + posi
+    if checkCollision() then
+      object.position.x = object.position.x - posi
+      object.gravity_time = 0
+    end
+    if love.keyboard.isDown("left") and object.speed["x-"] > 0 then
+      object.gravity_time = 0
+    end
+  elseif box.gravity.surface == "background" then
+    object.position.z = object.position.z - posi
+    if checkCollision() then
+      object.position.z = object.position.z + posi
+      object.gravity_time = 0
+    end
+    if love.keyboard.isDown("down") and object.speed["z+"] > 0 then
+      object.gravity_time = 0
+    end
+  end
+end
